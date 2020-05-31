f90239f193ac72b1a738ee3f6d5c05412c2b6724
Kasparas <kasparas.urbelis@ktu.edu>
2020-05-31 22:09:30 +0300

    small update

--- 4/lab4.pl
+++ 4/lab4.pl
@@ -1,9 +1,11 @@
+% 7.Suskaičiuokite sąrašo elementus, tuos, kurie nėra skaičiai
 count_letters([], 0).
 count_letters([H|T], N) :-
     count_letters(T, X),
     (number(H) -> N is X; N is X + 1).
 
 
+% 8.Rekursiškai suskaičiuokite sąrašų (bet kokio gylio) sveikų skaičių sumą
 sum_integers([], 0).
 sum_integers([H|T], N) :-
     sum_integers(T, X1),
